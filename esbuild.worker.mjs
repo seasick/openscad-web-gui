52ed2a14fb3fc81f1a6523e571cfccee48caaeda
Clemens Solar <git@clemens.solar>
2024-01-16 21:04:39 +0100

    fix: add openscad-wasm files to dist directory during build

--- esbuild.worker.mjs
+++ esbuild.worker.mjs
@@ -1,4 +1,5 @@
 /* eslint-env node */
+import { copy } from 'esbuild-plugin-copy';
 
 export default {
   entryPoints: ['src/openSCADWorker.mts'],
@@ -9,4 +10,16 @@ export default {
   target: 'es2022',
   format: 'esm',
   logLevel: 'info',
+  plugins: [
+    copy({
+      // this is equal to process.cwd(), which means we use cwd path as base path to resolve `to` path
+      // if not specified, this plugin uses ESBuild.build outdir/outfile options as base path.
+      resolveFrom: 'cwd',
+      assets: {
+        from: ['./src/vendor/openscad-wasm/*'],
+        to: ['./dist'],
+      },
+      watch: true,
+    }),
+  ],
 };
