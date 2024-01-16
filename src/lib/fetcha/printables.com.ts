e890c0e2a8ee9db41b8ee9aaf161ad7cddb2b075
Clemens Solar <git@clemens.solar>
2024-01-16 21:00:44 +0100

    fix: add corsproxy to printables url

--- src/lib/fetcha/printables.com.ts
+++ src/lib/fetcha/printables.com.ts
@@ -16,7 +16,7 @@ export default async function printablesComFetcha(
   const id: number = parseInt(idString, 10);
 
   const printProfileResponse = await fetch(
-    'https://api.printables.com/graphql/',
+    'https://corsproxy.io/?https://api.printables.com/graphql/',
     {
       headers: {
         Accept: 'application/json',
