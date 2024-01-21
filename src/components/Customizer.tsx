ae0fbd353130a4137666325e20041b21194f4085
Clemens Solar <git@clemens.solar>
2024-01-21 01:23:39 +0100

    feat: seperate parameter into groups, if available

--- src/components/Customizer.tsx
+++ src/components/Customizer.tsx
@@ -1,8 +1,10 @@
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import { Accordion, AccordionDetails, AccordionSummary } from '@mui/material';
 import Checkbox from '@mui/material/Checkbox';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormGroup from '@mui/material/FormGroup';
 import TextField from '@mui/material/TextField';
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import { Parameter } from '../lib/openSCAD/parseParameter';
 
@@ -29,44 +31,71 @@ export default function Customizer({ parameters, onChange }: Props) {
     onChange(newParameters);
   };
 
+  // Group parameters
+  const groups = useMemo(
+    () =>
+      parameters.reduce((acc, parameter) => {
+        if (parameter.group) {
+          if (!acc[parameter.group]) {
+            acc[parameter.group] = [];
+          }
+          acc[parameter.group].push(parameter);
+        } else {
+          acc[''] = acc[''] || [];
+          acc[''].push(parameter);
+        }
+        return acc;
+      }, {}) as { [key: string]: Parameters },
+    [parameters]
+  );
+
   return (
     <div style={{ height: '100%', overflow: 'scroll' }}>
-      {parameters.map((parameter) => {
-        if (parameter.type === 'number' || parameter.type === 'string') {
-          return (
-            <TextField
-              label={parameter.description || parameter.name}
-              fullWidth
-              type={parameter.type}
-              key={parameter.name}
-              name={parameter.name}
-              onChange={handleParameterChange}
-              value={parameter.value}
-              sx={{ mt: 2, p: 1 }}
-            />
-          );
-        } else if (parameter.type === 'boolean') {
-          return (
-            <FormGroup key={parameter.name}>
-              <FormControlLabel
-                control={
-                  <Checkbox
+      {Object.entries(groups).map(([groupName, groupParams], idx) => (
+        <Accordion defaultExpanded={idx === 0} key={idx}>
+          <AccordionSummary expandIcon={<ArrowDropDownIcon />}>
+            {groupName}
+          </AccordionSummary>
+          <AccordionDetails>
+            {groupParams.map((parameter) => {
+              if (parameter.type === 'number' || parameter.type === 'string') {
+                return (
+                  <TextField
+                    label={parameter.description || parameter.name}
+                    fullWidth
+                    type={parameter.type}
+                    key={parameter.name}
                     name={parameter.name}
                     onChange={handleParameterChange}
-                    checked={parameter.value === true}
+                    value={parameter.value}
+                    sx={{ mt: 2, p: 1 }}
                   />
-                }
-                label={parameter.description || parameter.name}
-              />
-            </FormGroup>
-          );
-        }
-        return (
-          <div key={parameter.name}>
-            {parameter.name} {parameter.type}
-          </div>
-        );
-      })}
+                );
+              } else if (parameter.type === 'boolean') {
+                return (
+                  <FormGroup key={parameter.name}>
+                    <FormControlLabel
+                      control={
+                        <Checkbox
+                          name={parameter.name}
+                          onChange={handleParameterChange}
+                          checked={parameter.value === true}
+                        />
+                      }
+                      label={parameter.description || parameter.name}
+                    />
+                  </FormGroup>
+                );
+              }
+              return (
+                <div key={parameter.name}>
+                  {parameter.name} {parameter.type}
+                </div>
+              );
+            })}
+          </AccordionDetails>
+        </Accordion>
+      ))}
     </div>
   );
 }
--- src/lib/openSCAD/parseParameter.ts
+++ src/lib/openSCAD/parseParameter.ts
@@ -3,44 +3,78 @@ export type Parameter = {
   type: 'string' | 'number' | 'boolean';
   value: string | boolean | number;
   description?: string;
+  group?: string;
 };
 
 export default function parseParameters(script: string): Parameter[] {
-  const parameters: Record<string, Parameter> = {};
-  const parameterRegex = /^(\w+)\s*=\s*([^;]+)/gm; // TODO: Use AST parser instead of regex
-
   // Limit the script to the upper part of the file. We don't want to parse the
   // entire file, just the parameters. This can be done by searching for the
   // first occurence of the `module` or `function` keyword.
   script = script.split(/^(module |function )/m)[0];
 
-  let match;
-  while ((match = parameterRegex.exec(script)) !== null) {
-    const name = match[1];
-    const value = match[2];
-    let description;
+  const parameters: Record<string, Parameter> = {};
+  const parameterRegex = /^(\w+)\s*=\s*([^;]+)/gm; // TODO: Use AST parser instead of regex
+  const groupRegex = /^\/\*\s*\[([^\]]+)\]\s*\*\//gm;
 
-    // Now search for the comment right above the parameter definition. This is done
-    // by splitting the script at the parameter definition and using the last line
-    // before the definition.
-    const splitted = script
-      .split(new RegExp(`^${escapeRegExp(match[0])}`, 'gm'))[0]
-      .trim()
-      .split('\n')
-      .reverse();
+  const groupSections: { id: string; group: string; code: string }[] = [];
+  let tmpGroup;
 
-    const lastLineBeforeDefinition = splitted[0];
-    if (lastLineBeforeDefinition.trim().startsWith('//')) {
-      description = lastLineBeforeDefinition.replace(/^\/\/\s*/, '');
-    }
-
-    // Using names as keys to avoid duplicates
-    parameters[name] = {
-      name,
-      description,
-      ...convertType(value),
-    };
+  // Find groups
+  while ((tmpGroup = groupRegex.exec(script))) {
+    groupSections.push({
+      id: tmpGroup[0],
+      group: tmpGroup[1].trim(),
+      code: '',
+    });
   }
+
+  // Add code to groupSections
+  groupSections.forEach((group, index) => {
+    const nextGroup = groupSections[index + 1];
+    const startIndex = script.indexOf(group.id);
+    const endIndex = nextGroup ? script.indexOf(nextGroup.id) : script.length;
+    group.code = script.substring(startIndex, endIndex);
+  });
+
+  // If there are no groups, add the entire script as a group
+  if (!groupSections.length) {
+    groupSections.push({
+      id: '',
+      group: undefined,
+      code: script,
+    });
+  }
+
+  groupSections.forEach((groupSection) => {
+    let match;
+    while ((match = parameterRegex.exec(groupSection.code)) !== null) {
+      const name = match[1];
+      const value = match[2];
+      let description;
+
+      // Now search for the comment right above the parameter definition. This is done
+      // by splitting the script at the parameter definition and using the last line
+      // before the definition.
+      const splitted = script
+        .split(new RegExp(`^${escapeRegExp(match[0])}`, 'gm'))[0]
+        .trim()
+        .split('\n')
+        .reverse();
+
+      const lastLineBeforeDefinition = splitted[0];
+      if (lastLineBeforeDefinition.trim().startsWith('//')) {
+        description = lastLineBeforeDefinition.replace(/^\/\/\s*/, '');
+      }
+
+      // Using names as keys to avoid duplicates
+      parameters[name] = {
+        description,
+        group: groupSection.group,
+        name,
+        ...convertType(value),
+      };
+    }
+  });
 
   return Object.values(parameters);
 }
